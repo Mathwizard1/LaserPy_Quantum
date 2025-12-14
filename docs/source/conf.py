f4b273f92f20a3d06ad4567955edd75599ce57ff
Mathwizard1 <anshurup.gupta@gmail.com>
2025-12-14 16:52:52 +0530

    docs setup

---
+++ docs/source/conf.py
@@ -0,0 +1,68 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+project = 'LaserPy_Quantum'
+copyright = '2025, Anshurup Gupta'
+author = 'Anshurup Gupta'
+release = '0.0.9'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',           # Google/NumPy docstrings (built-in!)
+    'sphinx.ext.viewcode',
+    #'sphinx.ext.intersphinx',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Intersphinx (cross-references) ------------------------------------------
+# intersphinx_mapping = {
+#     'python': ('https://docs.python.org/3', None),
+#     'numpy': ('https://numpy.org/doc/stable/', None),
+#     'matplotlib': ('https://matplotlib.org/stable/', None),
+# }
+
+# Napoleon settings (for Google/NumPy style docstrings)
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_use_ivar = True
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'undoc-members': True,
+    'exclude-members': '__weakref__',
+}
+
+add_module_names = False
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'      # Beautiful Read the Docs theme
+html_theme_options = {
+    'display_version': True,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+html_static_path = ['_static']
+#html_logo = '_static/logo.png'       # Add your logo here later
+#html_favicon = '_static/favicon.ico' # Add favicon later
+
+html_context = {
+    'display_github': True,
+    'github_user': 'Mathwizard1',
+    'github_repo': 'LaserPy_Quantum',
+    'github_version': 'main',
+    'github_url': '',
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,64 @@
+.. LaserPy_Quantum documentation master file, created by
+   sphinx-quickstart on Sun Dec 14 12:52:42 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+LaserPy_Quantum Documentation
+=============================
+
+Welcome to **LaserPy_Quantum** - Laser physics simulation utilities!
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Contents:
+
+API Reference
+=============
+
+.. automodule:: LaserPy_Quantum
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Components
+==========
+
+.. automodule:: LaserPy_Quantum.Components.Component
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. automodule:: LaserPy_Quantum.Components.Signal
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. automodule:: LaserPy_Quantum.Components.Simulator
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Specialized Components
+======================
+
+.. automodule:: LaserPy_Quantum.SpecializedComponents
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Photon
+======
+
+.. automodule:: LaserPy_Quantum.Photon
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Utilities
+=========
+
+.. automodule:: LaserPy_Quantum.utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
